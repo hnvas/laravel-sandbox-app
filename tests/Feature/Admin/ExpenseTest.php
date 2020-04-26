8dab96a95f3c2da930d977b854fedbe4210ced35
vasconcelosHenrique <henriquenvasconcelos@gmail.com>
2020-04-26 18:51:05 -0300

    Testes no crud de despesas

---
+++ tests/Feature/Admin/ExpenseTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Expense;
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExpenseTest extends TestCase
+{
+
+    use RefreshDatabase, WithFaker;
+
+    private static function indexExpenseRoute()
+    {
+        return route('expense.index');
+    }
+
+    private static function createExpenseRoute()
+    {
+        return route('expense.create');
+    }
+
+    private static function storeExpenseRoute()
+    {
+        return route('expense.store');
+    }
+
+    private static function editExpenseRoute($id)
+    {
+        return route('expense.edit', ['expense' => $id]);
+    }
+
+    private static function updateExpenseRoute($id)
+    {
+        return route('expense.update', ['expense' => $id]);
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->be(factory(User::class)->make());
+    }
+
+    public function testUserShouldViewListOfExpenses()
+    {
+        $this->get(self::indexExpenseRoute())
+             ->assertSuccessful()
+             ->assertViewIs('admin.expense.index');
+    }
+
+    public function testUserShouldViewCreateExpenseForm()
+    {
+        $this->get(self::createExpenseRoute())
+             ->assertSuccessful()
+             ->assertViewIs('admin.expense.create');
+    }
+
+    public function testUserShouldViewEditExpenseForm()
+    {
+        $expense = factory(Expense::class)->create();
+
+        $this->get(self::editExpenseRoute($expense->id))
+             ->assertSuccessful()
+             ->assertViewIs('admin.expense.edit');
+    }
+
+    public function testUserShouldCreateAnExpense()
+    {
+        $this->from(self::createExpenseRoute())
+             ->post(self::storeExpenseRoute(), [
+                 'amount'      => $this->faker->randomFloat(2, 1),
+                 'description' => 'Expense test creation',
+                 'due_date'    => $this->faker->date(),
+                 'issue_date'  => $this->faker->date(),
+                 'tags'        => [$this->faker->word()]
+             ])->assertRedirect(self::indexExpenseRoute())
+             ->assertSessionHas('success');
+    }
+
+    public function testUserShouldUpdateAnExpense()
+    {
+        $expense = factory(Expense::class)->create();
+
+        $this->from(self::editExpenseRoute($expense->id))
+             ->put(self::updateExpenseRoute($expense->id), [
+                 'amount'      => $this->faker->randomFloat(2, 1),
+                 'description' => 'Expense test creation',
+                 'due_date'    => $this->faker->date(),
+                 'issue_date'  => $this->faker->date(),
+                 'tags'        => [$this->faker->word()]
+             ])->assertRedirect(self::indexExpenseRoute())
+             ->assertSessionHas('success');
+    }
+}
